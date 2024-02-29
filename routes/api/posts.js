a63a63a83cc52bbfbe53cb7ee12c19cd6fc3ffef
devsanti1 <devsanti.dev@gmail.com>
2024-02-29 08:55:51 +0100

    delete post + commentaries

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -73,10 +73,11 @@ export default Router()
   .post('/delete/:id', async (req, res) => {
     try {
       if (req.session.user.username === decodeURIComponent((await models.Post.findById(new mongoose.Types.ObjectId(req.params.id))).author)) {
-        const postDeleted = await models.Post.findByIdAndDelete(new mongoose.Types.ObjectId(req.params.id))
+        await models.Post.findByIdAndDelete(new mongoose.Types.ObjectId(req.params.id))
+        await models.Commentary.deleteMany({ post_id: req.params.id })
         req.session.message = "Post Borrado exitosamente"
       } else {
-        req.sesssion.message = "Solo el autor del post puede eliminar el post"
+        req.session.message = "Solo el autor del post puede eliminar el post"
       }
     } catch (error) {
       req.session.message = "Ha ocurrido un error, el post no ha sido borrado"
